7582506af4d5e8caf46e35716e84a6c4c41d1a75
Troels Lund <trolund@gmail.com>
2022-09-16 19:30:51 +0200

    basic setup of pygame

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import pygame
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Basic setup
+pygame.font.init()
+pygame.mixer.init()
+
+FPS = 60
+DRONE_WIDTH, DRONE_HEIGHT = 55, 40
+
+WIDTH, HEIGHT = 1200, 900
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("DRONE SIMULATION - MBSE - GROUP 2 (2022)")
+BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Import resources
+DRONE_IMAGE = pygame.image.load(os.path.join('Assets', 'drone.svg'))
+DRONE = pygame.transform.rotate(pygame.transform.scale(DRONE_IMAGE, (DRONE_WIDTH, DRONE_HEIGHT)), 90)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def draw_window():
+    WIN.blit(DRONE, (0, 0))
+    pygame.draw.rect(WIN, BLACK, BORDER)
+    pygame.display.update()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def main():
+    # Simulation/game loop
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        draw_window()
+
+    main()
+
+
+if __name__ == "__main__":
+    main()
