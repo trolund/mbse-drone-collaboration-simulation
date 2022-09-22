66d45b13cedebb7114d4548ed3b44b48eedda2ed
Troels Lund <trolund@gmail.com>
2022-09-22 17:08:20 +0200

    basic UI

---
+++ UI.py
@@ -0,0 +1,4 @@
+class UI:
+
+    def __init__(self, packages=None, number_of_attachment_points=1):
+        print()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_gui
 
 # Basic setup
 from Models.ground import Ground
@@ -6,14 +7,18 @@ from Models.move_type import Move_Type
 from Models.task import Task
 from drone import Drone
 
+pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
 FPS = 60
 
 WIDTH, HEIGHT = 1200, 1000
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("DRONE SIMULATION - MBSE - GROUP 2 (2022)")
+pygame.font.Font(None, 22)
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
 
 # Colors
@@ -22,10 +27,12 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
+# UI
+UI_WIDTH = 400
+UI_X = WIN.get_width() - UI_WIDTH
 
-# Import resources
-# DRONE_IMAGE = pygame.image.load(os.path.join('Assets', 'drone.svg'))
-# DRONE = pygame.transform.rotate(pygame.transform.scale(DRONE_IMAGE, (DRONE_WIDTH, DRONE_HEIGHT)), 90)
+
+
 class Env:
     drones = pygame.sprite.Group()
     tasks = pygame.sprite.Group()
@@ -106,24 +113,63 @@ def draw_layers():
 
 
 def main():
+
+    margin = 30
+    ui_x = UI_X + margin
+
+    manager = pygame_gui.UIManager((WIN.get_width(), WIN.get_height()))
+
+    button1 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(ui_x, 400, 100, 30),
+        text='Click me 1',
+        manager=manager
+    )
+
+    button2 = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(ui_x, 200, 100, 30),
+        text='Click me 2',
+        manager=manager
+    )
+
+    label1 = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect(ui_x, 100, 200, 100),
+        text="Value",
+        manager=manager
+    )
+
     # create all objects in the environment
     create_env()
 
     # Simulation/game loop
     clock = pygame.time.Clock()
     run = True
+
     while run:
-        clock.tick(FPS)
+        time_delta = clock.tick(FPS) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
+            if event.type == pygame.USEREVENT:
+                if event.ui_element == button1:
+                        label1.set_text("click 1")
+                        print("click 1")
+                if event.ui_element == button2:
+                        label1.set_text("click 2")
+                        print("click 2")
+
+                manager.process_events(event)
         # clear background
         WIN.fill(WHITE)
 
         # draw object layers
         draw_layers()
+
+        # update and draw UI
+        pygame.draw.rect(WIN, (220, 220, 222), pygame.Rect(UI_X, 0, UI_WIDTH, WIN.get_height()))
+        manager.update(time_delta)
+        manager.draw_ui(WIN)
 
         # update screen with drawing
         draw_window()
