6d7d5c20af395c1023d086e8253955171b412632
Anders Haugen <69772191+anhau@users.noreply.github.com>
2022-10-14 10:54:35 +0200

    Started on this branch

--- Models/package.py
+++ Models/package.py
@@ -1,13 +1,18 @@
+from tokenize import String
+
+
 class Package:
 
-    wight: int
+    weight: int
     width: int
     height: int
+    address: String
 
-    def __init__(self, wight, width, height):
+    def __init__(self, wight, width, height, address):
         super().__init__()
-        self.wight = wight
+        self.weight = wight
         self.width = width
         self.height = height
-
+        self.address = address
+ 
 
