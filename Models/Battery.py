2ab0f89e60625d1aba18db8ef0e0cf340318e553
William Wulff <ww@skycore-semi.com>
2022-11-20 17:59:31 +0100

    Slight updates. Added comments.

--- Models/Battery.py
+++ Models/Battery.py
@@ -17,8 +17,8 @@ class Battery:
                  At: float = 0.1, # Cross-section [m^2], vertical
                  Cd: float = 1, # Drag coefficient
                  Ct: float = 0.03, # Thrust coefficient
-                 Ap: float = 0.5, # Propeller disc area [m^3]
-                 r: float = 0.05 # Propeller radius [m]
+                 Ap: float = 0.5, # Propeller disc area [m^2]
+                 r: float = 0.1 # Propeller radius [m]
                  ):
         self.md = md
         self.mp = mp
@@ -28,13 +28,17 @@ class Battery:
         self.Ct = Ct
         self.Ap = Ap
         self.r = r
-        
+
+    # Attach package to drone
+    # Update package parameters
     def set_package(self, mass, Apf, Apt):
         self.mp = mass
         self.Apf = Apf
         self.Apf = Apt
         print("Got package")
 
+    # Detach package from drone
+    # Set package parameters to 0
     def remove_package(self):
         self.mp = 0
         self.Apf = 0
@@ -47,12 +51,15 @@ class Battery:
 
         return ep
 
+    # Get power as function of rotor angular velocity
+    # Power approximated using 3rd degree polynomial
     def get_power(self, ang_freq: float):
         return 2.258 * 10**(-7) * ang_freq**3 + \
             3.866 * 10**(-5) * ang_freq**2 + \
             5.137 * 10**(-3) * ang_freq + \
             2.616
 
+    # Get power and energy consumption from movement
     def get_move_EP(self, dt, vv = 0, vh = 0):
         ang_freq_h = self.rot_horizontal(vh)
         ang_freq_v = self.rot_vertical(vv)
@@ -63,7 +70,8 @@ class Battery:
         E = P / dt
 
         return (E, P)
-        
+
+    # Calculate angular velocity for horizontal travel at vh m/s
     def rot_horizontal(self, vh: float):
         nom = ( 4*(self.md+self.mp)**2 * self.g**2 + \
                 self.rho**2 * (self.Af+self.Apf)**2 * self.Cd**2 * vh**4 )**(1/4)
@@ -71,6 +79,7 @@ class Battery:
 
         return nom/dnum
 
+    # Calculate angular velocity for vertical travel at vh m/s
     def rot_vertical(self, vv: float):
         nom = ( 2 * (self.md+self.mp) * self.g + \
                 self.rho * (self.At+self.Apt) * self.Cd * vv**2 )**(1/2)
@@ -81,6 +90,9 @@ class Battery:
 
     def conv_Ah(power, dt):
         return 0
+
+    def get_energy_consumed(self):
+        return self.battery_energy
 
     
 
