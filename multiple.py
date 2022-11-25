b280f0f24e7509b60b9a2e8b27c8a08d8bf5a1a3
Troels Lund <trolund@gmail.com>
2022-11-25 10:41:36 +0100

    read config

--- multiple.py
+++ multiple.py
@@ -1,21 +1,29 @@
-from containers import Container
-from simulation import Simulation
 import subprocess
 
+import configparser
 
 if __name__ == "__main__":
-    process = subprocess.Popen(['python3', 'simulation.py'],
-                               stdout=subprocess.PIPE,
-                               universal_newlines=True)
+    for x in range(1):
+        process = subprocess.Popen(['python3', 'simulation.py'],
+                                   stdout=subprocess.PIPE,
+                                   universal_newlines=True)
 
-    while True:
-        output = process.stdout.readline()
-        print(output.strip())
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            print('RETURN CODE', return_code)
-            # Process has finished, read rest of the output
-            for output in process.stdout.readlines():
-                print(output.strip())
-            break
+        config = configparser.RawConfigParser()
+        config.read("config.ini")
+        print("-----------------------------")
+        print(config.get("setup", "moving_truck"))
+        print("-----------------------------")
+
+        print(config)
+
+        while True:
+            output = process.stdout.readline()
+            print(output.strip())
+            # Do something else
+            return_code = process.poll()
+            if return_code is not None:
+                print('RETURN CODE', return_code)
+                # Process has finished, read rest of the output
+                for output in process.stdout.readlines():
+                    print(output.strip())
+                break
